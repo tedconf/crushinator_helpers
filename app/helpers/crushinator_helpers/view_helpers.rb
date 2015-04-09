db67d95b2e2b2850e6fe349ce71b0bdc05b0d4cb
Celly <celly@ted.com>
2015-04-09 14:24:33 -0400

    Move to akami CDN

--- app/helpers/crushinator_helpers/view_helpers.rb
+++ app/helpers/crushinator_helpers/view_helpers.rb
@@ -4,7 +4,15 @@ module CrushinatorHelpers
     def crushinate(url, options={})
       url = asset_path(url, digest: false, protocol: :request) if url
 
+      # This is for old possibly hardcoded Level3 urls. To keep from double crushing.
       if url.match(/img(?:-ssl)?\.tedcdn\.com\/r\//)
+        match_data = /(.+)?\/\/img(?:-ssl)?\.tedcdn\.com\/r\/([^?]+)\??(.*)/.match(url)
+        options = Rack::Utils.parse_nested_query(match_data[3]).symbolize_keys.merge(options.symbolize_keys)
+        url = "#{match_data[1]}//#{match_data[2]}"
+      end
+
+      # This is for possible new hardcoded Akami url's. To keep from double crushing
+      if url.match(/tedcdnpi-a.akamaihd.net\/r\//)
         match_data = /(.+)?\/\/img(?:-ssl)?\.tedcdn\.com\/r\/([^?]+)\??(.*)/.match(url)
         options = Rack::Utils.parse_nested_query(match_data[3]).symbolize_keys.merge(options.symbolize_keys)
         url = "#{match_data[1]}//#{match_data[2]}"
@@ -12,9 +20,9 @@ module CrushinatorHelpers
 
       if url.match(/(tedcdn|(images|storage|tedlive|tedlive-staging)\.ted|(s3|s3-us-west-2)\.amazonaws)\.com/)
         ssl_protocol_suffix = request.ssl? ? 's' : ''
-        ssl_subdomain = request.ssl? ? 'img-ssl' : 'img'
+        #ssl_subdomain = request.ssl? ? 'img-ssl' : 'img'
         url = url.gsub(/.*\/\//, '')
-        "http#{ssl_protocol_suffix}://#{ssl_subdomain}.tedcdn.com/r/#{url}?#{options.to_query}"
+        "http#{ssl_protocol_suffix}://tedcdnpi-a.akamaihd.net/r/#{url}?#{options.to_query}"
       else
         url
       end
